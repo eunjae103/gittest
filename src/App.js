1af0e9862fb4855e046a8d9290c73eac9eaeab6f
eunjae <>
2024-05-18 12:24:35 +0900

    브랜치하나 수정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Copyone from './Copyone';
 
 function App() {
   return (
     <div className="App">
      hello, world!
+     <Copyone/>
     </div>
   );
 }
