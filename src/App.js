7619fc680a60601f9cb414ffbff711b77598ae15
eunjae <>
2024-05-18 13:52:33 +0900

    브랜치합체

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import Copyone from './Copyone';
 
 function App() {
   return (
     <div className="App">
      hello, world!
-     <Copyone/>
     </div>
   );
 }
---
+++ src/Copytwo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Copytwo = () => {
+    return (
+        <div>
+            "카피투브랜치입니다"
+        </div>
+    );
+};
+
+export default Copytwo;
