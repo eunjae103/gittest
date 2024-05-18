01dea2a17503eb7f3551a05089a7d896d4a9c9d2
eunjae <>
2024-05-18 15:49:13 +0900

    합체

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Copyone from './Copyone';
+import Copytwo from './Copytwo';
 
 function App() {
   return (
     <div className="App">
      hello, world!
+     <Copytwo/>
+     <Copyone/>
     </div>
   );
 }
